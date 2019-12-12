36967b4919dc8a61d63e2f3f72f53f78921ceee9
Heatr <57447508+Heatr@users.noreply.github.com>
2019-12-12 10:00:32 +0100

    Add files via upload

--- Programme minimal/unitcombat.pas
+++ Programme minimal/unitcombat.pas
@@ -3,34 +3,47 @@ unit unitCombat;
 
 
 interface
-    uses unitPersonnage;
+    uses unitPersonnage,unitInventaire,GestionEcran;
 
   // TYPES
 
   //FONCTIONS ET PROCEDURES
 
-    procedure combat(var perso,monstre: personnage);
+    procedure combat(var perso,monstre: personnage;var inventairePerso : Inventaire;var fuite : Boolean);
 
 
 implementation
 
-procedure combat(var perso,monstre: personnage);
+procedure combat(var perso,monstre: personnage;var inventairePerso : Inventaire;var fuite : Boolean);
 
 var
    sortie: Boolean;
    choix: Integer;
    attaque: Integer;
+   rng : Integer;
 begin
 randomize;
 sortie := False;
+randomize();
+
        while sortie=FALSE DO
        begin
 
-
+         attendre(4000);
+         effacerEcran();
+         writeln('=============');
+         writeln('PV : ',perso.pv,' / ',perso.pvMax);
+         writeln('=============');
+         writeln();
+         writeln('=============');
+         writeln('PV Monstre : ',monstre.pv,' / ',monstre.pvMax);
+         writeln('=============');
+         writeln();
          writeln('Choisissez une option : ');
          writeln('1 - Attaquer');
          writeln('2 - Se defendre');
-         writeln('3 - Fuite');
+         writeln('3 - Utiliser une potion');
+         writeln('4 - Fuite');
          readln(choix);
          case choix of
               1:
@@ -56,8 +69,41 @@ sortie := False;
               end;
               3:
               begin
-                writeln('Vous vous enfuyez...');
-                sortie:=TRUE;
+                if inventairePerso.possession[3] = 0 then
+                  begin
+                  writeln('Vous N''avez pas de potion...');
+                  writeln('Choissiser une autre option.');
+                  end
+                else
+                  begin
+                  writeln('Vous consommer une potion !');
+                  writeln('Vous regagnez 30 PV !');
+                  inventairePerso.possession[3] := inventairePerso.possession[3]-1;
+                  perso.pv := perso.pv + 30;
+                  if perso.pv > perso.pvMax then
+                    perso.pv := perso.pvMax;
+                  attaque:= monstre.attaque + random(monstre.attaque div 2);
+                  perso.pv:= perso.pv - attaque;
+                  writeln();
+                  writeln(monstre.pseudo,' vous attaque, ',' vous subissez ',attaque,' pv. Il vous reste ',perso.pv,' pv.');
+                  end;
+              end;
+              4:
+              begin
+                rng := random(2);
+                if rng = 1 then
+                  begin
+                  writeln('Vous vous enfuyez !');
+                  sortie:=TRUE;
+                  fuite := True;
+                  end
+                else
+                  begin
+                  writeln('Vous n''avez pas reussi a vous enfuir...');
+                  attaque:= (monstre.attaque + random(monstre.attaque div 2)) - perso.defense;
+                  perso.pv:= perso.pv - attaque;
+                  writeln(monstre.pseudo,' vous attaque, ',' vous subissez ',attaque,' pv. Il vous reste ',perso.pv,' pv.');
+                  end;
               end;
        end;
          IF monstre.pv<=0 then
