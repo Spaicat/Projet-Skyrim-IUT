e89c1813accf8d482b56ac220252d91293bbff3c
Heatr <57447508+Heatr@users.noreply.github.com>
2019-12-22 13:34:53 +0100

    Commentaire

--- Programme principal/unitcombat.pas
+++ Programme principal/unitcombat.pas
@@ -33,7 +33,7 @@ var
    monstre : Personnage;
 
 begin
-     monstre.pseudo:= 'Illyar';
+     monstre.pseudo:= 'Illyar';         //On définit les differente statsitique des ennemies
      monstre.pv:= 120;
      monstre.pvMax:=120;
      monstre.attaque:=10;
@@ -47,7 +47,7 @@ var
    monstre : Personnage;
 
 begin
-     monstre.pseudo := 'Qjard';
+     monstre.pseudo := 'Qjard';         //On définit les differente statsitique des ennemies
      monstre.pv := 125;
      monstre.pvMax := 125;
      monstre.attaque := 11;
@@ -61,7 +61,7 @@ var
    monstre : Personnage;
 
 begin
-     monstre.pseudo := 'Ksiorn';
+     monstre.pseudo := 'Ksiorn';            //On définit les differente statsitique des ennemies
      monstre.pv := 130;
      monstre.pvMax := 130;
      monstre.attaque := 20;
@@ -72,7 +72,7 @@ function Ivrogne():Personnage;
 var
    monstre : Personnage;
 begin
-   monstre.pseudo := 'L''Ivrogne';
+   monstre.pseudo := 'L''Ivrogne';          //On définit les differente statsitique des ennemies
    monstre.pv := 75;
    monstre.pvMax := 75;
    monstre.attaque := 15;
@@ -105,12 +105,12 @@ begin
          setPersonnage(perso);
          InterfaceInGame();
          writelnPerso();
-         writelnPerso('=============');
+         writelnPerso('=============');                 //On affiche l'interface
          writelnPerso(' ' + monstre.pseudo + ' PV : ' + IntToStr(monstre.pv) + ' / ' + IntToStr(monstre.pvMax));
          writelnPerso('=============');
          writelnPerso();
 
-         writelnPerso('Choisissez une option : ');
+         writelnPerso('Choisissez une option : ');        //On sélectionne les option
          writelnPerso(' >  Attaquer');
          writelnPerso(' >  Se defendre');
          writelnPerso(' >  Utiliser une potion');
@@ -118,15 +118,15 @@ begin
          choix := selectionMenu(posXY(positionCurseur().x, positionCurseur().y-3), 4, 1, 2, LightBlue, White) + 1;
 
          case choix of
-              1:
+              1:            //Attaque
               begin
-                if effet = 1 then
+                if effet = 1 then           //Si etoudit
                   begin
                   writelnPerso('Vous etes etoudit');
                   rng := random(2);
                   if rng = 1 then
                     begin
-                    writelnPerso('Vous n''avez pas reussie a frapper...');
+                    writelnPerso('Vous n''avez pas reussie a frapper...');      //Cas ou l'étoudissement empèche d'attaquer
                     effetDuree := effetDuree + 1;
                     attaque:= monstre.attaque + random(monstre.attaque div 2);
                     if attaque < 0 then
@@ -139,7 +139,7 @@ begin
                     end
                   else
                     begin
-                    writelnPerso('Vous frapper malgre l''etourdissement');
+                    writelnPerso('Vous frapper malgre l''etourdissement');    //Cas ou on eux quand meme frapper
                     attaque:= perso.attaque + random(perso.attaque div 2);
                     monstre.pv := monstre.pv - attaque;
                     if monstre.pv <= 0 then
@@ -177,7 +177,7 @@ begin
                     end;
                   end;
               end;
-              2:
+              2:        //Defense
               begin
                 if effet = 1 then
                   begin
@@ -227,9 +227,9 @@ begin
                   writelnPerso(monstre.pseudo + ' vous attaque, ' + ' vous subissez ' + IntToStr(attaque) + ' pv. Il vous reste ' + IntToStr(perso.pv) + ' pv.');
                   end;
               end;
-              3:
+              3:      //Potion
               begin
-                if inventairePerso.possession[3] = 0 then
+                if inventairePerso.possession[3] = 0 then       //Vérifie que le joueur ai une potion
                   begin
                   writelnPerso('Vous N''avez pas de potion...');
                   writelnPerso('Choissiser une autre option.');
@@ -238,7 +238,7 @@ begin
                   begin
                   writelnPerso('Vous consommer une potion !');
                   writelnPerso('Vous regagnez 100 PV !');
-                  inventairePerso.possession[3] := inventairePerso.possession[3]-1;
+                  inventairePerso.possession[3] := inventairePerso.possession[3]-1;     //On la consomme
                   perso.pv := perso.pv + 100;
                   if perso.pv > perso.pvMax then
                     perso.pv := perso.pvMax;
@@ -256,19 +256,19 @@ begin
                     end;
                   end;
               end;
-              4:
+              4:            //Fuite
               begin
                 rng := random(2);
                 if rng = 1 then
                   begin
-                  writelnPerso('Vous vous enfuyez !');
+                  writelnPerso('Vous vous enfuyez !');      //Cas ou on reussie a fuir
                   sortie:= True;
                   fuite := True;
                   readlnPerso();
                   end
                 else
                   begin
-                  writelnPerso('Vous n''avez pas reussi a vous enfuir...');
+                  writelnPerso('Vous n''avez pas reussi a vous enfuir...');         //Cas ou on ne reussie pas a fuir
                   attaque:= (monstre.attaque + random(monstre.attaque div 2)) - perso.defense;
                   perso.pv:= perso.pv - attaque;
                   writelnPerso(monstre.pseudo + ' vous attaque, vous subissez ' + IntToStr(attaque) + ' pv. Il vous reste ' + IntToStr(perso.pv) + ' pv.');
@@ -281,7 +281,7 @@ begin
                   end;
               end;
        end;
-         if monstre.pv<=0 then
+         if monstre.pv<=0 then          //Si on gagne le combat
            begin
              sortie := True;
              writelnPerso('Vous avez gagnez !!');
@@ -289,7 +289,7 @@ begin
              perso.argent := perso.argent + monstre.argent;
              readlnPerso();
            end;
-         if perso.pv <= 0 then
+         if perso.pv <= 0 then          //Si on  plus de point de vie
            begin
            writelnPerso('Vous êtes mort...');
            readlnPerso();
